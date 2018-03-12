cb1666eadabc24badb376105c41f34c8719d8a56
Barld Boot <bakeds@gmail.com>
2018-03-12 16:59:06 +0100

    add functions for math

---
+++ FunctionalExtensions/FunctionalMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalExtensions
+{
+    public static class FunctionalMath
+    {
+        public static Func<double, double> DivideBy(double a) => (b) => b / a;
+        public static Func<float, float> DivideBy(float a) => b => b / a;
+    }
+}
